fc4a3b138c2f922277ef10aa834f3a873827d085
AdityaVenkatasundaram <85490617+AdityaVenkatasundaram@users.noreply.github.com>
2024-05-03 19:54:16 +1000

    Add JenkinsFile

---
+++ Jenkins-Pipeline.groovy
@@ -0,0 +1,60 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building the code using Maven'
+
+            }
+        }
+        stage('Unit and Integration Tests') {
+            steps {
+                echo 'Running unit tests'
+
+                echo 'Running integration tests'
+
+            }
+        }
+        stage('Code Analysis') {
+            steps {
+                echo 'Integrating code analysis tool'
+                echo 'Tool used: SonarQube'
+
+            }
+        }
+        stage('Security Scan') {
+            steps {
+                echo 'Performing security scan using OWASP Dependency-Check'
+
+            }
+        }
+        stage('Deploy to Staging') {
+            steps {
+                echo 'Deploying application to staging server using AWS CLI'
+                echo 'Tool used: AWS EC2'
+ 
+            }
+        }
+        stage('Integration Tests on Staging') {
+            steps {
+                echo 'Running integration tests on staging environment'
+                echo 'Tool used: Selenium'
+
+            }
+        }
+        stage('Deploy to Production') {
+            steps {
+                echo 'Deploying application to production server using AWS CLI'
+                echo 'Tool used: AWS EC2'
+
+            }
+            post{
+                success{
+                    mail to: "adityacalvin@gmail.com",
+                    subject: "Build Status Email",
+                    body: "Build was Successful!"
+                }
+            }
+        }
+    }
+}
